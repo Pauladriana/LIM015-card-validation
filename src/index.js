89d6e10446011b5c0a92fc03e815ea63ab837c55
Pauladriana <85115054+Pauladriana@users.noreply.github.com>
2021-06-08 22:39:54 -0500

    septima version

--- src/index.js
+++ src/index.js
@@ -9,38 +9,32 @@ const inputName = document.getElementById('holdername');
 const payButton = document.getElementById('submit')
 const goodCard = document.getElementById('valid');
 const badCard = document.getElementById('invalid');
-let isValid = '';
 const capa = document.getElementById('capa');
+const check = document.getElementById('bien');
+const equis = document.getElementById('mal');
 
 //BLOQUEANDO LAS TECLAS ALFABETICAS
 
 inputNumber.addEventListener("keypress", function(evt) {
     var ch = String.fromCharCode(evt.which);
-
     if(!(/[0-9]/.test(ch))) {
         evt.preventDefault();
     }
 });
-
 inputName.addEventListener("keypress", function(evt) {
     var ch = String.fromCharCode(evt.which);
-
     if(!(/[a-z A-Z]/.test(ch))) {
         evt.preventDefault();
     }
 });
-
 inputDate.addEventListener("keypress", function(evt) {
     var ch = String.fromCharCode(evt.which);
-
     if(!(/[0-9/]/.test(ch))) {
         evt.preventDefault();
     }
 });
-
 inputcvv.addEventListener("keypress", function(evt) {
     var ch = String.fromCharCode(evt.which);
-
     if(!(/[0-9]/.test(ch))) {
         evt.preventDefault();
     }
@@ -70,58 +64,21 @@ document.getElementById("cardnumber").addEventListener("change", function() {
             return num
         }
     })
-
     console.log(intoArray);
-
     //Ahora sumamos los componentes del array
-
     const totalnumber = intoArray.reduce((accumulator, currentvalue) => {
         return accumulator + currentvalue;
-    })
-
+    });
     console.log(totalnumber);
 
-    // Ahora validamos si es multimplo de diez
+    //APLICAMOS VALIDATOR.JS
 
-    function validez() {
-        const check = document.getElementById('bien');
-        const equis = document.getElementById('mal')
-        if (totalnumber % 10 == 0) {
-           check.style.display = 'block';
-           equis.style.display = 'none';
-           crdIncomplete.style.display = 'none';
-           isValid = 'yes'
-        } else {
-            check.style.display = 'none';
-            equis.style.display = 'block';
-            isValid = 'no'
+    validator.isValid(totalnumber);
 
-        }
+    let secret = inputNumber.value;
+    let secretNumber = secret.split('')
 
-    }
-
-    validez();
-
-
-
-    //OCULTAR CARACTERES
-    function asteriscos() {
-        let secret = inputNumber.value;
-        let secretNumber = secret.split('')
-        secretNumber = secretNumber.map((num,i) => {
-            if (i < (secretNumber.length - 4)) {
-                return '*';
-            } else {
-                return num
-            }
-        })
-    
-        let finalNumber = secretNumber.join('');
-        document.getElementById('cardnumber').value = finalNumber;
-        console.log(secret);
-    }
-
-    asteriscos()
+    validator.maskify(secretNumber);
 
 });
 
@@ -132,51 +89,47 @@ const dateIncomplete = document.getElementById('dtincomplete');
 const cvvIncomplete = document.getElementById('cvvincomplete');
 
 payButton.addEventListener("click", function() {
-
         if (inputNumber.value == '') {
             crdIncomplete.style.display = 'block'
         }
-
         if (inputName.value == '') {
             nameIncomplete.style.display = 'block'
         }
-
         if (inputDate.value == '') {
             dateIncomplete.style.display = 'block'
         }
-
         if (inputcvv.value == '') {
             cvvIncomplete.style.display = 'block'
         }
 
-        //PAGAR 
-        
-
-        if (inputNumber.value != '' && isValid == 'yes' && inputName.value != '' && inputDate.value != '' && inputcvv.value != '' ) {
+        //PAGAR
+        if (inputNumber.value != '' && check.style.display == 'block' && inputName.value != '' && inputDate.value != '' && inputcvv.value != '' ) {
             goodCard.style.display = 'block'
             capa.style.display = 'block'
         }
-
-        if (inputNumber.value != '' && isValid == 'no' && inputName.value != '' && inputDate.value != '' && inputcvv.value != '' ) {
+        if (inputNumber.value != '' && equis.style.display == 'block' && inputName.value != '' && inputDate.value != '' && inputcvv.value != '' ) {
             badCard.style.display = 'block'
             capa.style.display = 'block'
         }
 });
 
 
-
+inputNumber.addEventListener("change", function() {
+    if (inputNumber.value != '') {
+        crdIncomplete.style.display = 'none';
+    }
+})
 inputName.addEventListener("change", function() {
     if (inputName.value != '') {
         nameIncomplete.style.display = 'none';
+        console.log('funciona')
     }
 })
-
 inputDate.addEventListener("change", function() {
     if (inputName.value != '') {
         dateIncomplete.style.display = 'none';
     }
 })
-
 inputcvv.addEventListener("change", function() {
     if (inputName.value != '') {
         cvvIncomplete.style.display = 'none';
@@ -184,8 +137,6 @@ inputcvv.addEventListener("change", function() {
 })
 
 // CUADROS DE RESPUESTA
-
-
 document.getElementById('close1').addEventListener('click', function() {
     badCard.style.display = 'none';
     capa.style.display = 'none'
--- src/validator.js
+++ src/validator.js
@@ -1,6 +1,29 @@
 const validator = {
-  'is valid' : 'funcionaqui',
-  'maskify': 'funcionaqui'
+
+  isValid(creditCardNumber) {
+        const check = document.getElementById('bien');
+        const equis = document.getElementById('mal');
+        const crdIncomplete = document.getElementById('crdincomplete');
+        if (creditCardNumber % 10 == 0) {
+            check.style.display = 'block';
+            equis.style.display = 'none';
+            crdIncomplete.style.display = 'none';
+        } else {
+            check.style.display = 'none';
+            equis.style.display = 'block';
+        }
+  },
+  maskify(creditCardNumber) {
+    creditCardNumber = creditCardNumber.map((num,i) => {
+            if (i < (creditCardNumber.length - 4)) {
+                return '*';
+            } else {
+                return num
+            }
+        })
+        let finalNumber = creditCardNumber.join('');
+        document.getElementById('cardnumber').value = finalNumber;
+  }
 
 
   }
